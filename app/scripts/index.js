3afd7fa21508d4111bdbe0deba5fec3e8208ad7b
Quincy Chang <quincychang@Quincys-MacBook-Pro.local>
2018-12-10 15:01:32 +0800

    modify method setOnClickHandler

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -4,16 +4,17 @@ import { default as contract } from 'truffle-contract'
 import $ from 'jquery'
 import tictactoeArtifact from '../../build/contracts/TicTacToe.json'
 
-const TicTacToe = contract(tictactoeArtifact)
+const TicTacToe = contract(tictactoeArtifact);
+const Wager = Web3.utils.toWei('0.1', 'ether');
 
 let accounts
 let account
 let ticTacToe;
 
 window.App = {
-  start: function () {
-    const self = this
-    TicTacToe.setProvider(web3.currentProvider)
+	start: function () {
+		const self = this
+		TicTacToe.setProvider(web3.currentProvider)
 
 		// truffle bug - https://github.com/trufflesuite/truffle-contract/issues/57
 		if (typeof TicTacToe.currentProvider.sendAsync !== "function") {
@@ -24,40 +25,35 @@ window.App = {
 			};
 		}
 
-    web3.eth.getAccounts(function (err, accs) {
-      if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
-      }
-      if (accs.length === 0) {
-        alert("Couldn't get any accounts!")
-        return
-      }
+		web3.eth.getAccounts(function (err, accs) {
+			if (err != null) {
+				alert('There was an error fetching your accounts.')
+				return
+			}
+			if (accs.length === 0) {
+				alert("Couldn't get any accounts!")
+				return
+			}
 			console.log(accs);
-      accounts = accs
-      account = accounts[0]
-    })
-  },
+			accounts = accs
+			account = accounts[0]
+		})
+
+		// check web3 version
+		let oldWeb3 = web3.version.api;
+		let newWeb3 = web3.version;
+		console.log('web3 version :', oldWeb3 ? oldWeb3 : newWeb3);
+	},
 	useAccountOne: function() {
 		account = accounts[1];
+		console.log('choose account :', account);
 	},
 	createNewGame: function() {
 		console.log('Create Game Called, address :', account);
-		TicTacToe.new({
-			from: account,
-			value: web3.utils.toWei('0.1', 'ether'),
-			gas: 3000000
-
-		}).then(instance => {
+		TicTacToe.new({from: account, value: Wager, gas: 3000000}).then(instance => {
 			ticTacToe = instance;
-			console.log(ticTacToe);
-
-			// Set the On-Click Handler
-			for(var i = 0; i < 3; i++) {
-				for(var j = 0; j < 3; j++) {
-					$($('#board')[0].children[0].children[i].children[j]).off('click').click({x:i, y:j}, App.setStone);
-				}
-			}
+			console.log('contract address :', ticTacToe.address);
+			App.setOnClickHandler();
 
 		}).catch(error => {
 			console.log(error);
@@ -69,13 +65,11 @@ window.App = {
 		if(gameAddress != null) {
 			TicTacToe.at(gameAddress).then(instance => {
 				ticTacToe = instance;
-				return ticTacToe.joinGame({
-					from: account,
-					value: web3.utils.toWei('0.1', 'ether'),
-					gas: 3000000
-				});
+				return ticTacToe.joinGame({from: account,value: Wager,gas: 3000000});
+
 			}).then(txResult => {
-				console.log(txResult);
+				console.log('Next Player :', txResult.logs[1].args.player);
+				App.setOnClickHandler();
 
 			}).catch(error => {
 				console.log(error);
@@ -84,6 +78,16 @@ window.App = {
 	},
 	setStone: function(event) {
 		console.log(event);
+		ticTacToe.setStone(event.data.x, event.data.y, {from: account}).then(txResult => {
+			console.log(txResult);
+		});
+	},
+	setOnClickHandler: function() {
+		for(var i = 0; i < 3; i++) {
+			for(var j = 0; j < 3; j++) {
+				$($('#board')[0].children[0].children[i].children[j]).off('click').click({x:i, y:j}, App.setStone);
+			}
+		}
 	}
 };
 
